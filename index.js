6be29e4168dcf7d6200411770979e22d51734162
Tom Hohler <thohler@betomorrow.com>
2019-06-07 11:34:54 +0200

    Tslint + prettier

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import Home from './Home';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import Home from "./Home";
+import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => Home);
