3f6fd2936937f835bbde0e748e8905630ecfcf0c
Tom Hohler <thohler@betomorrow.com>
2019-06-09 11:22:51 +0200

    new contact screen

--- ui/screens/chatScreen.tsx
+++ ui/screens/chatScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export const ChatScreen = () => (
 	<SafeAreaView style={styles.container}>
