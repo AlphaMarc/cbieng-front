2466a8eee04044377285c50abdbbc46c74b914c3
Tom Hohler <thohler@betomorrow.com>
2019-06-13 17:12:28 +0200

    core and ios setup

--- android/app/src/main/java/com/cbieng/MainApplication.java
+++ android/app/src/main/java/com/cbieng/MainApplication.java
@@ -3,6 +3,7 @@ package com.cbieng;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new ReactNativeContacts(),
             new RNGestureHandlerPackage()
       );
