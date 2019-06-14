37d08d65dcb3cdcf43af391ac085f8ecc020a052
alphamarc <mallaire@betomorrow.com>
2019-06-14 14:01:19 +0200

    linked react-native-sound to play cbieng

--- android/app/src/main/java/com/cbieng/MainApplication.java
+++ android/app/src/main/java/com/cbieng/MainApplication.java
@@ -3,6 +3,7 @@ package com.cbieng;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSoundPackage(),
             new LinearGradientPackage(),
             new ReactNativeContacts(),
             new RNGestureHandlerPackage()
